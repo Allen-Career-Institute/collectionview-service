81ee7c1b480b02af847f7169f6374c93aaa3bd9d
shivanshhmani <shivanshmani.tripathi@allen.in>
2024-12-13 12:05:25 +0530

    Update constants.go
    Signed-off-by: shivanshhmani <shivanshmani.tripathi@allen.in>

--- internal/utils/constants.go
+++ internal/utils/constants.go
@@ -4,8 +4,8 @@ import "time"
 
 const (
 	MongoErrorTag         = "MONGO_ERROR"
-	Databasename          = "collection-view"
-	LibCollection         = "collection-repo"
+	Databasename          = "collectionview_db"
+	LibCollection         = "collectionview"
 	TTL                   = 36 * time.Hour
 	defaultSize           = 21
 	defaultAlphabet       = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
