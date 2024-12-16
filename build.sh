40a794f78e76fe89dc0d394a0c0a59b5062ce191
shivanshhmani <shivanshmani.tripathi@allen.in>
2024-12-16 12:51:17 +0530

    Create build.sh
    Signed-off-by: shivanshhmani <shivanshmani.tripathi@allen.in>

---
+++ build.sh
@@ -0,0 +1,12 @@
+# Configure Git with the provided GitHub Personal Access Token (GITHUB_PAT)
+echo 'Configuring Git...'
+git config --global --add safe.directory '*'
+git config --global url.https://$GITHUB_PAT@github.com/.insteadOf https://github.com/
+
+# Initialize the project (if necessary)
+echo 'Initializing the project...'
+make init
+
+# Build the project
+echo 'Building the project...'
+make build
