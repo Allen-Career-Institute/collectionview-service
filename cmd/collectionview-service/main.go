236567de50d2a6f476df0e4048680b850957706a
shivanshmani <shivanshmani.tripathi@gmail.com>
2024-12-16 17:35:26 +0530

    added stage config

--- cmd/collectionview-service/main.go
+++ cmd/collectionview-service/main.go
@@ -24,7 +24,7 @@ const (
 	DevEnv        = "dev"
 	StageEnv      = "stage"
 	ProdEnv       = "prod"
-	DockerConfDir = "/src/configs/"
+	DockerConfDir = "app/"
 	LocalConfDir  = "/Users/shivanshmani/Desktop/allen/collectionview-service/configs/"
 )
 
